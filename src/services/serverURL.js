f6752b7500e62c8d17b85390b5ee1c5de0c260ed
sonykuriakoseantony <sonykuriakoseantony@gmail.com>
2025-12-31 15:50:47 +0530

    vercel url resolve, updated server url

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,3 +1,5 @@
-const serverURL = "http://localhost:3000"
+// const serverURL = "http://localhost:3000";
+const serverURL = "https://bookstore-mern-stack-server.onrender.com";
+
 
 export default serverURL;
